74f663416475b903d7e1b1c351dac6e6c6421557
github username <github email address>
2020-03-01 19:24:08 -0700

    Add login page seperate from create user page and adjust styling of those two pages

--- app.js
+++ app.js
@@ -1,48 +1,54 @@
  const createForm = document.querySelector('#login-create') 
  const loginForm = document.querySelector('#login')
+ const signupUrl = "http://localhost:3000/users"
+ const authUrl = "http://localhost:3000/login"
 
  createForm.addEventListener('submit', () => {
+    event.preventDefault()
+    console.log(event)
     const formData = new FormData(createForm)
     const username = formData.get('username')
-    const name = formData.get('password')
+    const password = formData.get('password')
  
-    fetch('http://localhost:3000/users', {
+    fetch(signupUrl, {
         method: 'POST',
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({username, password})
-    }).then(response => response.json())
-    .then(response => {
-        localStorage.setItem("token", response.token)
-})
+    }).then(response => { 
+        if(response.status >= 400) throw new Error("Bad Request")
+        window.location.href='index.html'
+    }).catch(error => console.error(error.message))
 }) 
 
 loginForm.addEventListener('submit', () => {
+    event.preventDefault()
     const formData = new FormData(loginForm)
     const username = formData.get('username')
-    const name = formData.get('password')
+    const password = formData.get('password')
  
-    fetch('http://localhost:3000/users', {
-        method: 'GET',
+    fetch(authUrl, {
+        method: 'POST',
         headers: {
             "Content-Type": "application/json"
-        },
+        }, 
         body: JSON.stringify({username, password})
     }).then(response => response.json())
+   .then( window.location.href="page_1.html")
     .then(response => {
         localStorage.setItem("token", response.token)
-})
+    })
 }) 
 
-const token = localStorage.getItem("token")
+// const token = localStorage.getItem("token")
 
-fetch('http://localhost:3000/users', {
-    method: "GET", 
-    headers: {
-        "Authorization": `Bearer ${token}`
-    }
-})
+// fetch('http://localhost:3000/users', {
+//     method: "GET", 
+//     headers: {
+//         "Authorization": `Bearer ${token}`
+//     }
+// })
 
 
 
